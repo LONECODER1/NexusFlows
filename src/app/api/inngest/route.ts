ec150c9a456c5cbec71acda98f465cffc642cd98
LONECODER1 <adityagupta1112004vns@gmail.com>
2026-08-07 16:29:14 +0530

    Node seclector completed

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,11 @@
-// src/app/api/inngest/route.ts
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { processTask } from "./functions";
+import { executeWorkflow } from "@/inngest/functions";
 
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
-    client: inngest,
-    functions: [processTask],
+  client: inngest,
+  functions: [
+    executeWorkflow,
+  ],
 });
